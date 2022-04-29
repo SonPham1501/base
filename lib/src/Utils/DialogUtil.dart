b850a12d4f2e49c1d2b0dcdb000b5a61e2dd44ce
SON <son@SONs-Mac-mini.local>
2022-04-29 14:58:07 +0700

    init base

--- lib/src/Utils/DialogUtil.dart
+++ lib/src/Utils/DialogUtil.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class DialogUtil {
-  static void showLoading() {
+  static showLoading() {
     // Future.delayed(Duration.zero, () async {
     //   Get.dialog(
     //     WillPopScope(
