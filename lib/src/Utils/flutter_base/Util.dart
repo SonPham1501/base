67f7d5f97066fbd77ba7d4e7594d934d7fba54a9
SON <son@SONs-Mac-mini.local>
2022-06-23 10:39:45 +0700

    click polygon

--- lib/src/Utils/flutter_base/Util.dart
+++ lib/src/Utils/flutter_base/Util.dart
@@ -13,6 +13,7 @@ import 'package:flutter_native_image/flutter_native_image.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:html/parser.dart';
 import 'package:intl/intl.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:path/path.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:uuid/uuid.dart';
@@ -818,6 +819,21 @@ class Util {
   //tắt bàn phím
   static void onHideKeyboard(BuildContext context) {
     FocusScope.of(context).requestFocus(FocusNode());
+  }
+
+  //chọn polygon
+  static bool isGeoPointInPolygon(LatLng l, List<LatLng> polygon) {
+    var isInPolygon = false;
+
+    for (var i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+      if ((((polygon[i].latitude <= l.latitude)
+        && (l.latitude < polygon[j].latitude))
+        || ((polygon[j].latitude <= l.latitude)
+        && (l.latitude < polygon[i].latitude))) 
+        && (l.longitude < (polygon[j].longitude - polygon[i].longitude) * (l.latitude - polygon[i].latitude)
+          /(polygon[j].latitude - polygon[i].latitude) + polygon[i].longitude)) isInPolygon = !isInPolygon;
+    }
+    return isInPolygon;
   }
 
 }
