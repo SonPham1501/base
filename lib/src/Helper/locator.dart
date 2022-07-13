a589d90cc69b1b80319518e656d3543731078fc7
SON <son@SONs-Mac-mini.local>
2022-07-13 10:07:05 +0700

    injection

--- lib/src/Helper/locator.dart
+++ lib/src/Helper/locator.dart
@@ -1,9 +1,9 @@
 import 'package:base/base.dart';
 import 'package:get_it/get_it.dart';
 
-final GetIt locator = GetIt.instance;
+GetIt locator = GetIt.instance;
 
 void setupLocator() {
   locator.registerLazySingleton(() => NavigationService());
-  locator.registerLazySingleton(() => GraphQLApiClient(uri: locator()));
+  locator.registerLazySingleton(() => GraphQLApiClient(uri: AppBase.apiUrl));
 }
