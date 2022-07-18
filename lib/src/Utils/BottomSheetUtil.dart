1eb0f58932b1857198c87ef1c6888863d1c729f4
SON <son@SONs-Mac-mini.local>
2022-07-18 11:11:09 +0700

    bottomsheet

--- lib/src/Utils/BottomSheetUtil.dart
+++ lib/src/Utils/BottomSheetUtil.dart
@@ -5,12 +5,14 @@ abstract class BottomSheetUtil {
   static Future<T?> buildBaseButtonSheet<T>({
     required Widget child,
     bool isScroll = true,
+    Color? color,
   }) async {
     BuildContext _context = navigationService.context;
     final T? _res = await showModalBottomSheet(
       context: _context,
       useRootNavigator: true,
       isScrollControlled: isScroll,
+      backgroundColor: color,
       builder: (c) => child,
     );
     return _res;
@@ -20,12 +22,14 @@ abstract class BottomSheetUtil {
     required Widget child,
     double ratio = 0.9,
     bool isScroll = true,
+    Color? color,
   }) async {
     BuildContext _context = navigationService.context;
     final T? _res = await showModalBottomSheet(
       context: _context,
       useRootNavigator: true,
       isScrollControlled: isScroll,
+      backgroundColor: color,
       builder: (c) => SizedBox(
         height: MediaQuery.of(_context).size.height * ratio,
         width: double.infinity,
